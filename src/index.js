5d2f0463a2addeff8d51caf8e8d536c76f953c8d
ViktorGuschin <groftblr@gmail.com>
2019-10-03 00:07:01 +0300

    work: 4 pass

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/*module.exports =*/ function zeros(expression) {
+/*module.exports = */ function zeros(expression) {
   'use strict';
   const factorials = expression.split('*');
   let zeroCount = 0;
@@ -8,19 +8,41 @@
   const isOdd = item => item % 2 !== 0;
 
   const zerosInFactorial = (item, double = false, odd = false) => {
-    let count = 0;
-    // const factor = double ? 10 : 5;
-    // if (odd) {
-    //   item--;
-    // } else if (double) {
-    //   item /= 2;
-    // }
-    // item = double ? item / 2 : item;
-    while (item > 0) {
-      item = Math.floor(item / 5);
-      count += item;
+    function zeroCount(number) {
+      let resMax = 0;
+      let resMin = 0;
+
+      let index = 5;
+      while (index <= number) {
+        resMax += Math.floor(number / index);
+        index *= 5;
+      }
+      index = 2;
+      while (index <= number) {
+        resMin += Math.floor(number / index);
+        index *= 2;
+      }
+      return Math.min(resMin, resMax);
     }
-    return count;
+
+    if (double) {
+      let count = 0;
+      for (let i = item; i >= 0; i = i - 2) {
+        count += zeroCount(i);
+      }
+      // if (odd) {
+      //   for (let i = 2; i <= item; i + 2) {
+      //     count += zeros(i);
+      //   }
+      // } else {
+      //   for (let i = 1; i <= item; i + 2) {
+      //     count += zeros(i);
+      //   }
+      // }
+      return count;
+    } else {
+      return zeros(item);
+    }
   };
 
   factorials.forEach(el => {
@@ -31,11 +53,12 @@
     // );
     if (isDouble(el)) {
       el = el.slice(0, -2);
-      if (!isOdd(el)) {
-        zeroCount += zerosInFactorial(+el + 1, true);
-      } else {
-        zeroCount += zerosInFactorial(el, true, true);
-      }
+      zeroCount += zerosInFactorial(el, true);
+      // if (!isOdd(el)) {
+      //   zeroCount += zerosInFactorial(el, true);
+      // } else {
+      //   zeroCount += zerosInFactorial(el, true, true);
+      // }
     } else {
       el = el.slice(0, -1);
       zeroCount += zerosInFactorial(el);
@@ -44,5 +67,6 @@
   return zeroCount;
 }
 
-zeros('1!!*2!!*3!!*4!!*5!!*6!!*7!!*8!!*9!!*10!!');
+// zeros('1!!*2!!*3!!*4!!*5!!*6!!*7!!*8!!*9!!*10!!');
 // zeros('1!*2!*3!*4!*5!*6!*7!*8!*9!*10!');
+console.log(zeros('9!!*10!!*7!!'));
